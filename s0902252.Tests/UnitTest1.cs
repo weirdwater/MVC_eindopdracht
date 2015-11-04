804b2bfd96bbedb6e14e037cc7f5702e5a035e65
Arjo <arjo@weirdwater.net>
2015-11-04 09:24:08 +0100

    The start of a 32 hour journey.

---
+++ s0902252.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace s0902252.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
